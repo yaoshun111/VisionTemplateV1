70514054be5043c5b4e0a2967823f497a3052eea
yaoshunCo <928687514@qq.com>
2020-03-21 11:44:02 +0800

    Revert "dd"
    This reverts commit 2abefbf0dc36219ab6d663f59c4b54609d3ddcf8.

--- UIform/ProductType.Designer.cs
+++ UIform/ProductType.Designer.cs
@@ -110,7 +110,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(233, 350);
+            this.button1.Location = new System.Drawing.Point(220, 350);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 43;
